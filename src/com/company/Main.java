efacf5aa213636a2b140a6a7f3de08a233f45b63
Thushani <9thushani7@gmail.com>
2022-03-14 23:12:01 +0530

    14/03/2022 - Encapsulation 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    employee e = new employee();
+        e.setID(34);
+        e.setName("Thushi");
+        e.setSalary(250000);
+
+        System.out.println("Name is "+ e.getName());
+    }
+}
